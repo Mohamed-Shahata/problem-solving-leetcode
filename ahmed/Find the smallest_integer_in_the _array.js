602009e0fa7f6af91ee3b90c4ba009bf072d1cf0
ahmed mohamed <ahmed.muhamed.tech@gmail.com>
2026-04-08 01:11:06 +0200

    Find the smallest_integer_in_the _array

---
+++ ahmed/Find the smallest_integer_in_the _array.js
@@ -0,0 +1,8 @@
+function findSmallestInt(arr) {
+  let minNumber = arr[0];
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] < minNumber)
+        minNumber = arr[i];
+  }
+  return minNumber;
+}
